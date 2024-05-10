44c831a7399bdbab5facb0c2ac95fb60de4973e0
Luan Melo <luanmelolima@gmail.com>
2024-05-09 22:07:47 -0300

    Corrigindo o tailwindcss

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,9 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="bg-zinc-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: '#FFF',
+    fontWeight: '700',
+    fontSize: 48,
+  },
+});
