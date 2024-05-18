87c099ce01a06115e31fd881ca97c8330bee9678
Luan Melo <luanmelolima@gmail.com>
2024-05-17 22:20:34 -0300

    adicionando as estrelas no front end

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 min-h-screen">
+    <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 border-r border-white/10">
+      <div className="relative bg-[url(../assets/bg-stars.svg)] bg-cover flex flex-col items-start justify-between overflow-hidden px-28 py-16 border-r border-white/10">
       {/* Blur */}  
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
       {/* Stripes */}
@@ -10,7 +10,7 @@ export default function Home() {
       </div>
 
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
