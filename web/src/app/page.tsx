e03afea39883243f7cde61d12d345e52b487560e
Luan Melo <luanmelolima@gmail.com>
2024-05-18 08:45:47 -0300

    adicionando a parte do Hero

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -23,7 +23,29 @@ export default function Home() {
           </p>
         </a>
 
-        
+        {/* Hero */} 
+        <div className="space-y-5">
+          <Image src={nlwlogo} alt="NLW Spacetime"/>
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+            </p>
+          </div>
+
+          <a 
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-all text-sm uppercase leading-none text-black hover:bg-green-600" 
+            href=""
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+      
+         
       </div>
 
       {/* Right */}
