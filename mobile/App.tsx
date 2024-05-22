2f9edb98fae5f4e96e52b3895e6a452f6ec98d09
Luan Melo <luanmelolima@gmail.com>
2024-05-22 20:14:27 -0300

    adicionando layout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Text} from 'react-native';
+import { ImageBackground, View, Text, TouchableOpacity} from 'react-native';
 
 import {
   useFonts,
@@ -12,6 +12,11 @@ import {
 } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -28,8 +33,29 @@ export default function App() {
     <ImageBackground 
     source={blurBg}
       className="bg-gray-900 flex-1 items-center justify-center"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
     >
-        <Text className="text-gray-50 font-bold text-5xl">RocketSeat</Text>
+      <StyledStripes className="absoluto left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6" >
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="">
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3">
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
         <StatusBar style="light" translucent />
     </ImageBackground>
   )
