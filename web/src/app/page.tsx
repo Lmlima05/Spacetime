9c677b52a3550c87ca39870ebdac45e3ff91e55d
Luan Melo <luanmelolima@gmail.com>
2024-06-17 22:09:08 -0300

    adicionando componente Profile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,14 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
+import { cookies } from 'next/headers'
 
 import { Copyright } from '@/components/Copyright'
 import { Hero } from '@/components/Hero'
 import { Signin } from '@/components/Signin'
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { Profile } from '@/components/profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -15,7 +17,8 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
       {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-        <Signin />
+        
+        {isAuthenticated ? <Profile /> : <Signin />}
         <Hero />
         <Copyright />  
       </div>
