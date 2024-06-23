72ccf3ee8928268f1a5a3f2b9a9343513de554d2
Luan Melo <luanmelolima@gmail.com>
2024-06-23 20:39:46 -0300

    Obtendo o Github code mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -15,15 +15,49 @@ import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/Ov23liQjYaC7Dp6zoiiu',
+};
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, singInWithGithub] = useAuthRequest(
+    {
+      clientId: 'Ov23liQjYaC7Dp6zoiiu',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      })
+    },
+    discovery
+  )
+
+  useEffect(() => {
+    // Esse código serve para pegar o ip da máquina 
+    //para cadastrar no github para autenticar o token
+   // console.log(
+  //  makeRedirectUri({
+   //   scheme: 'nlwspacetime'
+   //   })
+  //)
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      console.log(code)
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null
@@ -49,10 +83,14 @@ export default function App() {
           </Text>
         </View>
 
-        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3">
-          <Text className="font-alt text-sm uppercase text-black">
-            Cadastrar lembrança
-          </Text>
+        <TouchableOpacity 
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => singInWithGithub()}
+        >  
+            <Text className="font-alt text-sm uppercase text-black">
+              Cadastrar lembrança
+            </Text>
         </TouchableOpacity>
       </View>
 
