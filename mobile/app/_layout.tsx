d6f100d1b6f4b1494bfdac584c6ac0d8ee76a6f7
Luan Melo <luanmelolima@gmail.com>
2024-07-11 21:14:30 -0300

    ajustando o Layout.tsx

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,21 @@
+import { styled } from 'nativewind'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+
+import { ImageBackground } from "react-native";
+
+const StyledStripes = styled(Stripes)
+
+export default function layout() {
+  return (
+    <ImageBackground 
+      source={blurBg}
+      className="relative bg-gray-900 flex-1 items-center px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+    </ImageBackground>
+
+  )
+}
