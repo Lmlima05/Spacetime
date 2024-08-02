9754066d326f1b33b9706c0268eb94ac803a7083
Luan Melo <luanmelolima@gmail.com>
2024-08-02 20:46:40 -0300

    Rota upload de arquivos e servindo arq estaticos

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,4 +1,9 @@
-import { FastifyInstance } from 'fastify';
+import { randomUUID } from 'node:crypto'
+import { extname, resolve } from 'node:path'
+import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request) => {
@@ -19,6 +24,20 @@ export async function uploadRoutes(app: FastifyInstance) {
       return reply.status(400).send()
     }
 
-    console.log(upload.filename)
+    const fileId = randomUUID()
+    const extension = extname(upload.filename)
+
+    const fileName = fileId.concat(extension)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads/', fileName),
+    )
+
+    const pump(upload.file, writeStream)
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname)
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return { fileUrl }
   });
 }
