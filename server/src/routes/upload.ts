8921789f4232b61f12e20c6a6c68ac6f03042f03
Luan Melo <luanmelolima@gmail.com>
2024-08-01 22:06:27 -0300

    testando o arquivo upload

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -2,5 +2,23 @@ import { FastifyInstance } from 'fastify';
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request) => {
+    const upload = await request.file({
+      limits: {
+        fileSize: 5_242_880, // 5mb
+      },
+    })
+
+    if (!upload) {
+      return reply.status(400).send()
+    }
+
+    const mimeTypeRegex = /ˆ(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+    if(isValidFileFormat) {
+      return reply.status(400).send()
+    }
+
+    console.log(upload.filename)
   });
 }
