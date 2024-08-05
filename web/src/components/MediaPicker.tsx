aa7c7b5a450eb16b106c69bfcd3dccd2fa582202
Luan Melo <luanmelolima@gmail.com>
2024-08-04 22:01:25 -0300

    permitindo para aceitar comente imagenf

--- web/src/components/MediaPicker.tsx
+++ web/src/components/MediaPicker.tsx
@@ -1,15 +1,35 @@
 'use client'
 
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 export function MediaPicker() {
-  function onFileSelect(event: ChangeEvent<HTMLInputElemnt>) {}
+  const [previw, setPreviw] = useState<string | null>(null)
+
+  function onFileSelect(event: ChangeEvent<HTMLInputElemnt>) {
+    const { files } = event.target
+  }
+
+  if (!files) {
+    return
+  }
 
 return (
+  <>
   <input
     onChange={onFileSelected}
     type="file"
-    id="media"
+    id="media"  
+    accept="image/*"
     className="invisible h-0 w-0"
   />
-)}
+
+  {preview && (
+    <img 
+      src={preview} 
+      alt="" 
+      className="aspect-video w-full rounded-lg object-cover"
+    />
+  )}
+  </>
+)
+}
