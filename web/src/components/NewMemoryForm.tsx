3dca0be34f02e2c5f6716d7d9499ce714e051b97
Luan Melo <luanmelolima@gmail.com>
2024-08-06 22:32:02 -0300

    adicionando o newmemohrforms para um novo arquivo

---
+++ web/src/components/NewMemoryForm.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Camera } from "lucide-react";
+import { MediaPicker } from "./MediaPicker"
+
+export function NewMemoryFrom() {
+  return (
+    <form className="flex flex-1 flex-col gap-2">
+        <div className="flex items-center gap-4">
+          <label
+            htmlFor="media"
+            className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+          >
+            <Camera className="h-4 w-4"/>
+            Anexar mídia
+          </label>
+
+          <label
+            htmlFor="isPublic"
+            className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+          >
+            <input type="checkbox" name="isPublic" id="isPublic" value="true" className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"/>
+            tornar memória pública   
+          </label>  
+        </div>
+
+        <MediaPicker />
+
+        <textarea
+          name="content"
+          spellCheck={false}
+          className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />  
+      </form>
+  )
+}
