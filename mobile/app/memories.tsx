7f7faae26ce67a56de1882833f9bb2723e923f31
Luan Melo <luanmelolima@gmail.com>
2024-08-12 19:43:14 -0300

    modificando o memories.tsx

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,28 @@
-import { Text, View } from 'react-native'
+import { ScrollView, TouchableOpacity, View } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
 
-export default function Memories() {
-  return (
-    <View className="flex-1 items-center jutify-center">
-      <Text>Memories</Text>
-    </View>
-  )
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
+return (
+  <ScrollView
+    className="flex-1 px-8"
+    contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+>
+  <View className="mt-4 flex-row items-center justify-between">
+    <NLWLogo />
+
+    <Link href="/memories" asChild>
+      <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full">
+        <Icon name="arrow-left" size={16} color="#FFF" />
+      </TouchableOpacity>  
+    </Link>
+  </View>  
+</ScrollView>
+
+)
 }
