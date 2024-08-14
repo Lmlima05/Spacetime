b540b4320c86dedeaa5dbab19dbc49b032899898
Luan Melo <luanmelolima@gmail.com>
2024-08-14 20:02:34 -0300

    Botão de logout

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,12 +1,20 @@
 import { ScrollView, TouchableOpacity, View } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
+import * as SecureStore from 'expo-secure-store'
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+
+async function signOut() {
+  await SecureStore.deleteItemAsync('token')
+  
+  router.push('/')
+}
 
 return (
   <ScrollView
@@ -17,8 +25,8 @@ return (
     <NLWLogo />
 
     <View className="flex-row gap-2">
-      <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
-        <Icon name="plus" size={16} color="#000" />
+      <TouchableOpacity onPress={signOut} className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+        <Icon name="log-out" size={16} color="#000" />
       </TouchableOpacity>
     </View>
 
